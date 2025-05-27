a189ff57b22a72ec30a17529edb3e5d4030545b9
정재영 <wodud04277@gmail.com>
2025-05-27 22:17:17 +0900

    [Feat] Player Move, Jump

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float jumpForce = 10f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private bool isGrounded;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -14,19 +16,31 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
         }
     }
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * movement.normalized);
+        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
     }
     private void Jump()
     {
-        Debug.Log("Jump!");
+        rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
     }
-    
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
 }
