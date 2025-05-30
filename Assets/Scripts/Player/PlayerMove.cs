79446ddb1bc266a202819a91ff6f9289c3c71723
정재영 <wodud04277@gmail.com>
2025-05-30 10:04:37 +0900

    [Feat] Home Player Create
    GameHomeScene전용 Player를 생성함. 둘이 Animator도 다름. 이 Player는 키 입력을 받아 움직일 수 있음.

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -9,25 +9,47 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
     private bool isGrounded;
+    private Animator animator;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsIdle", true);
     }
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.F) && isGrounded)
         {
             Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
         }
     }
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+        if (movement.x != 0)
+        {
+            animator.SetBool("IsIdle", false);
+            animator.SetBool("IsRun", true);
+        }
+        else
+        {
+            animator.SetBool("IsIdle", true);
+            animator.SetBool("IsRun", false);
+        }
     }
     private void Jump()
     {
         rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        animator.SetTrigger("Jump");
+    }
+    private void Attack()
+    {
+        animator.SetTrigger("Attack");
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
